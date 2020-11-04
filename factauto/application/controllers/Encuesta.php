0eea96a8fcde3b7f1760ca34dd593886c5eab9f5
Alvarez Muñoz, S <s.alvarez.munoz@accenture.com>
2020-11-03 23:28:46 -0300

    mejoras en el front

--- factauto/application/controllers/Encuesta.php
+++ factauto/application/controllers/Encuesta.php
@@ -1,7 +1,48 @@
 <?php
 
 class Encuesta extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();  
+        $this->load->helper('form');
+        $this->load->library('form_validation');      
+    }
+
     public function responder(){
+        if($this->input->post('resp1')){
+            $resp1 = $this->input->post('resp1');
+            $resp2 = $this->input->post('resp2');
+            $resp3 = $this->input->post('resp3');
+
+            $username = $this->session->userdata('username');
+
+            $opts = array('http' =>
+                array(
+                    'method'  => 'POST',
+                    'header'  => 'Content-type: application/json',
+                    'content' => '{        
+                        "resp1": "'.$resp1.'",
+                        "resp2": "'.$resp2.'",
+                        "resp3": "'.$resp3.'",
+                        "username": "'.$username.'"
+                }
+                '
+                )
+            );
+
+             # Create the context
+             $context = stream_context_create($opts);
+             # Get the response (you can use this for GET)
+             $result = file_get_contents($this->config->item('base_api').'responder', false, $context);
+             
+             $retorno = json_decode($result);
+ 
+             if($retorno->status == "success"){
+                 $this->session->set_flashdata('exito', "se ha ingresado las respuestas correctamente");                
+             }else{
+                 $this->session->set_flashdata('error', $retorno->message);                
+             }
+        }
         $this->load->view('templates/header');
         $this->load->view('encuesta/responder');
         $this->load->view('templates/footer');
--- factauto/application/views/encuesta/responder.php
+++ factauto/application/views/encuesta/responder.php
@@ -1,28 +1,42 @@
 <div class="card card-primary">
+
+        <?php if($this->session->flashdata('error')){ ?>
+            <div class="alert alert-danger" role="alert">
+            <?= $this->session->flashdata('error'); ?>
+            </div>
+        <?php } ?>
+
+        <?php if($this->session->flashdata('exito')){ ?>
+            <div class="alert success" >
+            <?= $this->session->flashdata('exito'); ?>
+            </div>
+        <?php } ?>
+
+
     <div class="card-header">
     <h3 class="card-title">Responder Encuesta</h3>
     </div>
     <!-- /.card-header -->
     <!-- form start -->
-    <form role="form">
+    <?= form_open('Encuesta/responder'); ?>
     <div class="card-body">
         <div class="form-group">
             <label for="exampleInputEmail1">Pregunta 1: ¿Que le gustaria que agregaramos al informe?</label>
-            <textarea class="form-control" rows="5" placeholder="Su respuesta ..."></textarea>
+            <textarea class="form-control" rows="5" name="resp1" placeholder="Su respuesta ..."></textarea>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Pregunta 2: ¿La información es correcta? </label>
             <div class="form-group">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp2" value="Si">
                     <label class="form-check-label">Si</label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp2" value="No" >
                     <label class="form-check-label">No</label>
                 </div>  
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp2" value="Mas o menos">
                     <label class="form-check-label">Mas o menos</label>
                 </div>                
             </div>
@@ -32,23 +46,23 @@
             <label for="exampleInputPassword1">Pregunta 3: ¿De 1 a 5, es rápido nuestro sitio ?</label>
             <div class="form-group">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp3" value="1">
                     <label class="form-check-label">1</label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp3" value="2">
                     <label class="form-check-label">2</label>
                 </div>  
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp3" value="3">
                     <label class="form-check-label">3</label>
                 </div>  
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp3" value="4">
                     <label class="form-check-label">4</label>
                 </div>  
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="radio1">
+                    <input class="form-check-input" type="radio" name="resp3" value="5">
                     <label class="form-check-label">5</label>
                 </div>                
             </div>
